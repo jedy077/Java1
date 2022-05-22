9c7d4fb220b7271173a6444ca0e12d42499ab7d4
jedy077 <barami05@naver.com>
2022-05-22 13:50:19 +0900

    fiinish chap09

---
+++ chap09-package/BigIntegerEx.java
@@ -0,0 +1,23 @@
+import java.math.*;
+
+class BigIntegerEx{
+	public static void main (String[] args) {
+		for(int i=1; i < 100 ; i++ ){
+			System.out.printf("%d!=%s%n", i, calcFactorial(i));
+			try {
+				Thread.sleep(300);
+			} catch (Exception e) {}
+		}
+	}
+
+	static String calcFactorial(int n) {
+		return factorial(BigInteger.valueOf(n)).toString();
+	}
+
+	static BigInteger factorial (BigInteger n ) {
+		if(n.equals(BigInteger.ZERO))
+			return BigInteger.ONE;
+		else
+			return n.multiply(factorial(n.subtract(BigInteger.ONE)));
+	}
+}
