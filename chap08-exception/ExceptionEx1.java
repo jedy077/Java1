050a8720b84260f3fa8b4c3beb49e51a9d0131d5
jedy077 <barami05@naver.com>
2022-05-24 21:47:15 +0900

    chap10 start

---
+++ chap08-exception/ExceptionEx1.java
@@ -0,0 +1,13 @@
+class ExceptionEx1 {
+	public static void main (String[] args) {
+		try {
+			try { } catch (Exception e) { }
+		} catch (Exception e) {
+			tyr { } catch (excpetion e) { }  /// error 발생 e 를 다른 변수로 변경해야 함. 
+		} 
+
+		try {
+		} catch (Excpetion e) {
+		}
+	}
+}
